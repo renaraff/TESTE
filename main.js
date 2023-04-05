1c46c891963de9ad2e0c6a87241c026ca9111c5e
Renara <129889661+renaraff@users.noreply.github.com>
2023-04-05 12:30:52 -0300

    divisao e multiplicacao

--- main.js
+++ main.js
@@ -29,13 +29,13 @@ function soma ()
 
 function divisao ()
 {
-    let num1 = document.getElementById( "num3" ).value;
-    let num2 = document.getElementById( "num4" ).value;
+    let num3 = document.getElementById( "num3" ).value;
+    let num4 = document.getElementById( "num4" ).value;
     document.getElementById("resultado2").innerHTML = parseFloat(num3) / parseFloat(num4);
 }
 function multiplicar ()
 {
-    let num1 = document.getElementById( "num5" ).value;
-    let num2 = document.getElementById( "num6" ).value;
+    let num5 = document.getElementById( "num5" ).value;
+    let num6 = document.getElementById( "num6" ).value;
     document.getElementById( "resultado3" ).innerHTML = parseFloat(num5) * parseFloat(num6);
 }
