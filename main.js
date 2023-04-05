4cde5b1e6d240e5a9b43a619e2a49ff22a717fc7
Renara <129889661+renaraff@users.noreply.github.com>
2023-04-05 12:04:15 -0300

    alterações

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ document.addEventListener( "click" , showcolor );
 
 document.getElementById("botao").addEventListener("click" , exibenome )
 
-
+document.getElementById( "btn-soma").addEventListener("click" , soma)
 function showcolor ()
 {
     document.body.style.backgroundColor = "red";
@@ -13,4 +13,11 @@ function exibenome()
     let usuario = document.getElementById( "nome" ).value;
     document.getElementById( "mensagem" ).innerHTML = "Meu nome é: " + usuario;
     document.getElementById( "nome" ).value = "";
+}
+
+function soma ()
+{
+    let num1 = document.getElementById( "num1" ).value;
+    let num2 = document.getElementById( "num2" ).value;
+    document.getElementById("resultado1").innerHTML = parseFloat(num1) + (num2);
 }
