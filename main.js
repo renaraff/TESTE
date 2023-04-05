ede5cd12e25ddc67b5da3bef8cf9f0ea1350d510
Renara <129889661+renaraff@users.noreply.github.com>
2023-04-05 12:19:50 -0300

    alteração divisão

--- main.js
+++ main.js
@@ -3,6 +3,9 @@ document.addEventListener( "click" , showcolor );
 document.getElementById("botao").addEventListener("click" , exibenome );
 
 document.getElementById( "btn-soma" ).addEventListener( "click" , soma );
+
+document.getElementById( "btn-divisao" ).addEventListener( "click" , divisao );
+
 function showcolor ()
 {
     document.body.style.backgroundColor = "red";
@@ -20,4 +23,11 @@ function soma ()
     let num1 = document.getElementById( "num1" ).value;
     let num2 = document.getElementById( "num2" ).value;
     document.getElementById("resultado1").innerHTML = parseFloat(num1) + parseFloat(num2);
+}
+
+function divisao ()
+{
+    let num1 = document.getElementById( "num3" ).value;
+    let num2 = document.getElementById( "num4" ).value;
+    document.getElementById("resultado2").innerHTML = parseFloat(num3) + parseFloat(num4);
 }
