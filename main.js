8bcf2aa0cce7433027840c8a536ddbb1460511fa
Renara <129889661+renaraff@users.noreply.github.com>
2023-04-05 11:27:19 -0300

    nome value

--- main.js
+++ main.js
@@ -11,4 +11,5 @@ function exibenome()
 {
     let usuario = document.getElementById( "nome" ).value;
     document.getElementById( "mensagem" ).innerText = "Meu nome é: " + usuario;
+    document.getElementById( "nome" ).value = "";
 }
