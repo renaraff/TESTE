c86b904694c82b9f5986576a0b88496fa8f636dc
Renara <129889661+renaraff@users.noreply.github.com>
2023-04-05 12:09:00 -0300

    alteração no java

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 document.addEventListener( "click" , showcolor );
 
-document.getElementById("botao").addEventListener("click" , exibenome )
+document.getElementById("botao").addEventListener("click" , exibenome );
 
-document.getElementById( "btn-soma").addEventListener("click" , soma)
+document.getElementById( "btn-soma" ).addEventListener( "click" , soma );
 function showcolor ()
 {
     document.body.style.backgroundColor = "red";
@@ -19,5 +19,5 @@ function soma ()
 {
     let num1 = document.getElementById( "num1" ).value;
     let num2 = document.getElementById( "num2" ).value;
-    document.getElementById("resultado1").innerHTML = parseFloat(num1) + (num2);
+    document.getElementById("resultado1").innerHTML = parseFloat(num1) + parseFloat(num2);
 }
