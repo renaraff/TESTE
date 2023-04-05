314c5b5bb9d1bf352cb7a147514e2e9b12c1872c
Renara <129889661+renaraff@users.noreply.github.com>
2023-04-05 11:11:10 -0300

    Javascript alteração

--- main.js
+++ main.js
@@ -9,5 +9,6 @@ function showcolor ()
 
 function exibenome()
 {
-    
+    let usuario = document.getElementById( "nome" ).value;
+    document.getElementById( "mensagem" ).innerText = "Meu nome é: " + usuario;
 }
